37d0d81f936341f08b1970768164878f68f2d8e3
Felix Knecht <github@felixknecht.de>
2014-07-02 19:43:30 +0800

    erster release

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 auto-tatort
 ===========
 
-Kleines Script um die aktuelle Tatort Folgen automatisiert (cron) aus der ARD Mediathek zu laden
+Kleines Script um die aktuelle Tatort Folgen automatisiert (cron) aus der ARD Mediathek zu laden.
+
+Bitte editieren und den Pfad anpassen in dem die Dateien abgelegt werden sollen. Die Vergabe des Names erfolgt automatisch und basiert auf dem Titel des RSS Feed Eintrags.
+
+Dann Script jeden Abend um 23:50 via cron automatisiert ausfuehren lassen:
+
+50 23 * * * python ~/bin/auto-tatort/autoTatort.py
+
+Das Script laed nur Folgen aus dem RSS Feed, die auch das Datum des aktuellen Tages haben. Das hat den Vorteil, dass die ganzen Dokumentationen und Vorschauen etc nicht geladen werden. Diese werden naemlich meistens mit einem vergangenen Datum eingestellt. Die Filme werden irgendwann kurz nach der Sendung in den Feed eingebaut. Daher sollte es eigentlich immer mit 23:50 klappen.
+
+Abhaengigkeiten
+===============
+
+import sys
+import feedparser (apt-get install python-feedparser)
+import datetime
+import urlparse
+from urllib import urlopen, urlretrieve
+import json
+
+Wurde nicht mit Python3 getestet, sollte aber vorher mit dem 2to3 Tool konvertiert werden!
---
+++ autoTatort.py
@@ -0,0 +1,47 @@
+import sys
+import feedparser
+import datetime
+import urlparse
+from urllib import urlopen, urlretrieve
+import json
+
+RSS_URL = "http://www.ardmediathek.de/tv/Tatort/Sendung?documentId=602916&bcastId=602916&rss=true"
+
+#1=512x288
+#2=640x360
+#3=960x544
+QUALITY = 3
+TARGET_DIR = "/data/tatort/"
+
+
+feed = feedparser.parse( RSS_URL )
+
+items = feed.entries
+
+today = datetime.date.today()
+#today = datetime.date(2014,06,23)
+
+for item in items:
+   year = item["date_parsed"][0];
+   month = item["date_parsed"][1];
+   day = item["date_parsed"][2];
+   feedDate = datetime.date(item["date_parsed"][0], item["date_parsed"][1], item["date_parsed"][2])
+
+   if feedDate == today:
+      title = item["title"]
+      link = item["link"]
+      parsed = urlparse.urlparse(link)
+      docId = urlparse.parse_qs(parsed.query)['documentId']
+      docUrl = 'http://www.ardmediathek.de/play/media/' + docId[0] + '?devicetype=pc&features=flash'
+
+      response = urlopen(docUrl)
+      html = response.read()
+
+      media = json.loads(html)
+      mediaLinks = media["_mediaArray"][1]["_mediaStreamArray"]
+      for mediaLink in mediaLinks:
+         if QUALITY == mediaLink["_quality"]:
+            mediaURL = mediaLink["_stream"]
+            fileName = "".join([x if x.isalnum() or x in "- " else "" for x in title])
+            urlretrieve(mediaURL, TARGET_DIR + fileName + ".mp4")
+
