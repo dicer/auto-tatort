4fdd5b81f6a2b08eae8b8fe1839f81132b158886
Felix Knecht <github@felixknecht.de>
2016-01-08 21:25:29 +0100

    Tighter indention and got rid of "christmas trees"

--- autoTatort.py
+++ autoTatort.py
@@ -41,82 +41,86 @@ today = datetime.date.today()
 
 
 for item in items:
-   year = item["date_parsed"][0];
-   month = item["date_parsed"][1];
-   day = item["date_parsed"][2];
-   feedDate = datetime.date(item["date_parsed"][0], item["date_parsed"][1], item["date_parsed"][2])
+  year = item["date_parsed"][0];
+  month = item["date_parsed"][1];
+  day = item["date_parsed"][2];
+  feedDate = datetime.date(item["date_parsed"][0], item["date_parsed"][1], item["date_parsed"][2])
 
-   if feedDate == today:
-      title = item["title"]
-      link = item["link"]
-      parsed = urlparse.urlparse(link)
-      docId = urlparse.parse_qs(parsed.query)['documentId']
-      docUrl = 'http://www.ardmediathek.de/play/media/' + docId[0] + '?devicetype=pc&features=flash'
+  if feedDate != today:
+    continue
 
-      response = urlopen(docUrl)
-      html = response.read()
+  title = item["title"]
+  link = item["link"]
+  parsed = urlparse.urlparse(link)
+  docId = urlparse.parse_qs(parsed.query)['documentId']
+  docUrl = 'http://www.ardmediathek.de/play/media/' + docId[0] + '?devicetype=pc&features=flash'
 
-      if 'http://www.ardmediathek.de/-/stoerung' == response.geturl():
-        print "Could not get item with title '" + title + "'. Got redirected to '" + response.geturl() + "'. This is probably because the item is still in the RSS feed, but not available anymore."
-        continue
+  response = urlopen(docUrl)
+  html = response.read()
 
-      try:
-        media = json.loads(html)
-      except ValueError as e:
-        print e
-        print "Could not get item with title '" + title + "'. Original item link is '" + link + "' and parsed docId[0] is '" + docId[0] + "', but html response from '" + docUrl + "' was '" + html + "'"
-        continue
+  if 'http://www.ardmediathek.de/-/stoerung' == response.geturl():
+    print "Could not get item with title '" + title + "'. Got redirected to '" + response.geturl() + "'. This is probably because the item is still in the RSS feed, but not available anymore."
+    continue
 
-      if '_mediaArray' not in media or len(media["_mediaArray"]) == 0:
-        print "Skipping " + title + " because it does not have any mediafiles"
-        continue
-      mediaLinks = media["_mediaArray"][1]["_mediaStreamArray"]
+  try:
+    media = json.loads(html)
+  except ValueError as e:
+    print e
+    print "Could not get item with title '" + title + "'. Original item link is '" + link + "' and parsed docId[0] is '" + docId[0] + "', but html response from '" + docUrl + "' was '" + html + "'"
+    continue
 
-      downloadQuality = QUALITY
+  if '_mediaArray' not in media or len(media["_mediaArray"]) == 0:
+    print "Skipping " + title + " because it does not have any mediafiles"
+    continue
+  mediaLinks = media["_mediaArray"][1]["_mediaStreamArray"]
 
-      #get best quality?
-      if downloadQuality == -1:
-        downloadQuality = 0
-        for mediaLink in mediaLinks:
-          if mediaLink["_quality"] > downloadQuality and '_stream' in mediaLink:
-            downloadQuality = mediaLink["_quality"]
+  downloadQuality = QUALITY
+
+  #get best quality?
+  if downloadQuality == -1:
+    downloadQuality = 0
+    for mediaLink in mediaLinks:
+      if mediaLink["_quality"] > downloadQuality and '_stream' in mediaLink:
+        downloadQuality = mediaLink["_quality"]
 
 
-      downloadedSomething = 0
+  downloadedSomething = 0
 
-      for mediaLink in mediaLinks:
-         if downloadQuality == mediaLink["_quality"]:
-            stream = mediaLink["_stream"]
-            mediaURL = ""
-            #check if the selected quality has two stream urls
-            if type(stream) is list or type(stream) is tuple:
-              if len(stream) > 1:
-                mediaURL = stream[1]
-              else:
-                mediaURL = stream[0]
-            else:
-              mediaURL = stream
+  for mediaLink in mediaLinks:
+    if downloadQuality != mediaLink["_quality"]:
+      continue
 
-            fileName = "".join([x if x.isalnum() or x in "- " else "" for x in title])
-            urlretrieve(mediaURL, TARGET_DIR + fileName + ".mp4")
-            downloadedSomething = 1
-            print "Downloaded '" + title + "'"
+    stream = mediaLink["_stream"]
+    mediaURL = ""
+    #check if the selected quality has two stream urls
+    if type(stream) is list or type(stream) is tuple:
+      if len(stream) > 1:
+        mediaURL = stream[1]
+      else:
+        mediaURL = stream[0]
+    else:
+      mediaURL = stream
 
-            #download subtitles
-            try:
-              if SUBTITLES and '_subtitleUrl' in media and len(media["_subtitleUrl"]) > 0:
-                offset = 0
-                if '_subtitleOffset' in media:
-                 offset = media["_subtitleOffset"]
+    fileName = "".join([x if x.isalnum() or x in "- " else "" for x in title])
+    urlretrieve(mediaURL, TARGET_DIR + fileName + ".mp4")
+    downloadedSomething = 1
+    print "Downloaded '" + title + "'"
 
-                subtitleURL = 'http://www.ardmediathek.de/' + media["_subtitleUrl"]
-                urlretrieve(subtitleURL, TARGET_DIR + fileName + "_subtitleOffset_" + str(offset) + ".xml")
-            except Exception as e:
-              #print and resume with download
-              print e
-              print subtitleURL
+    #download subtitles
+    try:
+      if SUBTITLES and '_subtitleUrl' in media and len(media["_subtitleUrl"]) > 0:
+        offset = 0
+        if '_subtitleOffset' in media:
+          offset = media["_subtitleOffset"]
 
-      #check whether we download something
-      if downloadedSomething == 0:
-        print "Could not download '" + title + "' because of an error or nothing matching your quality selection"
+        subtitleURL = 'http://www.ardmediathek.de/' + media["_subtitleUrl"]
+        urlretrieve(subtitleURL, TARGET_DIR + fileName + "_subtitleOffset_" + str(offset) + ".xml")
+    except Exception as e:
+      #print and resume with download
+      print e
+      print subtitleURL
+
+  #check whether we download something
+  if downloadedSomething == 0:
+    print "Could not download '" + title + "' because of an error or nothing matching your quality selection"
 
